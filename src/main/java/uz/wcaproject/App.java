c3d59df01f374b848e87d0989cfb21756e592560
unknown <mahmudovmuhammadumar25@gmail.com>
2025-11-01 10:04:34 +0500

    'project/initialization'

---
+++ src/main/java/uz/wcaproject/App.java
@@ -0,0 +1,13 @@
+package uz.wcaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
