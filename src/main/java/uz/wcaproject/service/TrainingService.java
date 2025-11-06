353222f1af3d1d087a72993539d81ae283eec3da
mahmudovmuhammadumar25 <muhammadumar_mahmudov@epam.com>
2025-11-06 16:32:09 +0500

    implemented storage and dao clasess

--- src/main/java/uz/wcaproject/service/TrainingService.java
+++ src/main/java/uz/wcaproject/service/TrainingService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uz.wcaproject.dao.TrainingDAO;
 import uz.wcaproject.model.Training;
 
 import java.util.List;
